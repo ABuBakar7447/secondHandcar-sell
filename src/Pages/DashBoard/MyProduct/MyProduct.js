3d4ea106a34a6193cbb2c656ec348b0e1ce50a2f
ABuBakar7447 <lone7447wolf@gmail.com>
2022-11-30 16:27:56 +0600

    seller can remove theri product now

--- src/Pages/DashBoard/MyProduct/MyProduct.js
+++ src/Pages/DashBoard/MyProduct/MyProduct.js
@@ -1,25 +1,62 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../AuthProvider/AuthProvider';
 import MyProductCard from './MyProductCard';
 
 const MyProduct = () => {
-    const {user} = useContext(AuthContext)
+    const {user} = useContext(AuthContext);
 
-    const {data : product= []} = useQuery({
-        queryKey: ['product'],
-        queryFn: async() => {
-            const res = await fetch(`http://localhost:5000/product?seller_email=${user?.email}`);
-            const data = await res.json();
-            return data;
+
+    const [productDetails, setProductDetails] = useState([])
+    console.log(productDetails)
+
+    //collecting data base on user email
+    useEffect(() => {
+        fetch(`http://localhost:5000/product?seller_email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setProductDetails(data))
+    }, [user?.email])
+
+
+    // const {data : product= []} = useQuery({
+    //     queryKey: ['product'],
+    //     queryFn: async() => {
+    //         const res = await fetch(`http://localhost:5000/product?seller_email=${user?.email}`);
+    //         const data = await res.json();
+    //         return data;
+    //     }
+    // })
+
+
+    const haldleDelete = id => {
+        const proceed = window.confirm('Do you want to remove your product?');
+        if (proceed) {
+            fetch(`http://localhost:5000/delproduct/${id}`, {
+                method: 'Delete'
+            })
+
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('your product deleted successfully')
+                        const leftProDet = productDetails.filter(del => del._id !== id);
+                        setProductDetails(leftProDet)
+                    }
+                })
         }
-    })
+    }
+
+
+
     return (
         <div>
             <div className='w-11/12 grid gap-6 gird-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto my-5 '>
                     {
-                        product.map(sellerProduct =><MyProductCard key={sellerProduct._id}
-                            sellerProduct={sellerProduct}></MyProductCard>
+                        productDetails.map(sellerProduct =><MyProductCard key={sellerProduct._id}
+                            sellerProduct={sellerProduct}
+                            haldleDelete={haldleDelete}
+                            ></MyProductCard>
                         )
                     }
             </div>
--- src/Pages/DashBoard/MyProduct/MyProductCard.js
+++ src/Pages/DashBoard/MyProduct/MyProductCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MyProductCard = ({sellerProduct}) => {
+const MyProductCard = ({sellerProduct, haldleDelete}) => {
 
     const {img_url,product_name, location, resale_price, original_price, post_date, seller_name, product_status, _id} = sellerProduct
     return (
@@ -18,7 +18,7 @@ const MyProductCard = ({sellerProduct}) => {
                     <h3 className='font-bold'>Product Status: <span className='text-yellow-400'>{product_status}</span></h3>
 
                     <div className=' md:flex lg:flex-row flex-col m-5 justify-center'>
-                    <button className='font-bold btn btn-error m-5'>Delete Product</button>
+                    <button  onClick={()=>haldleDelete(_id)} className='font-bold btn btn-error m-5'>Delete Product</button>
                     <button className="font-bold btn btn-warning m-5">Advertise Product</button>
                     </div>
 
